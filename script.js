03854fe8dbd066b4ae80f6407ab4828cb7b0681e
Pignataro67 <Pignataro67@gmail.com>
2022-04-25 09:59:50 -0700

    JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
 async function generateJoke() {
   const config = {
     headers: {
